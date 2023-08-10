a07fd91b5a4f7931283402974c98730c3eb73d6a
Jibril <jibrilchettaoui@gmail.com>
2023-08-10 11:39:28 +0200

    - Added AdminCodesView and the buttons and modals that go with it

--- custom_views.py
+++ custom_views.py
@@ -1,7 +1,10 @@
-from discord import ButtonStyle, User, Interaction, Embed, Color
-from discord.ui import View, Button
+from discord import ButtonStyle, User, Interaction, Embed, Color, InputTextStyle
+from discord.ui import View, Button, Modal, InputText
+
+from json import dump as json_dump
 
 from main import db, get_parameter, get_text
+from custom_functions import get_codes_data
 
 curA = db.cursor(buffered=True)
 curB = db.cursor(buffered=True)
@@ -290,3 +293,183 @@ class ShopPerksButton(Button):  #"lang
         emb.add_field(name=get_text("shop.global_items", self.lang), value=text, inline=False)
 
         await interaction.response.edit_message(embed=emb, view=ShopBrowserView(category='perks', lang=self.lang))
+
+
+#######################################
+#                                     #
+#              ADMIN COG              #
+#                                     #
+#######################################
+
+###############################
+#         ADMIN CODES         #
+###############################
+
+class AdminCodesView(View):
+    def __init__(self, pages: list = None):
+        super().__init__(
+            timeout=None
+        )
+        self.pages = []
+
+        # discontinued -> will be replaced by commands
+        # Add code
+        # Edit code
+        # Remove code
+
+        # Previous page -> add page number in embed
+        self.add_item(AdminCodesCloseButton())
+        self.add_item(AdminCodesRefreshButton())
+        self.add_item(AdminCodesToggleButton())
+        self.add_item(AdminCodesDetailButton())
+        # Next page
+
+
+class AdminCodesModal(Modal):
+    def __init__(self, title: str, cb) -> None:
+        super().__init__(
+            title=title
+        )
+        self.cb = cb
+
+        self.add_item(InputText(label="Input codes separated by commas"))
+
+    async def callback(self, interaction: Interaction):
+        codes = self.children[0].value.split(',')
+        await self.cb(interaction, codes)
+
+
+class AdminCodesCloseButton(Button):
+    def __init__(self):
+        super().__init__(
+            label="Close",  #get_text("view.button.close", ""),
+            style=ButtonStyle.red,
+            custom_id="admin:codes:close"
+        )
+
+    async def callback(self, interaction: Interaction):
+        await self.view.message.delete()
+
+
+class AdminCodesRefreshButton(Button):
+    def __init__(self):
+        super().__init__(
+            label="Refresh",  #get_text("view.admin.codes.refresh", ""),
+            style=ButtonStyle.grey,
+            custom_id="admin:codes:refresh"
+        )
+
+    async def callback(self, interaction: Interaction):
+
+        codes = get_codes_data()
+
+        resume = []
+        for code in codes:
+            start = f"• {code}{(16 - len(code)) * ' '} :"
+            count = f"{codes[code]['usage_count']}/{codes[code]['usage_limit'][0]}"
+            mode = f"({codes[code]['usage_limit'][1]})"
+            disabled = f"{(6 - len(mode)) * ' '}not" if codes[code]['disabled'] else ''
+            resume.append(f"{start} {count} {(10 - len(count)) * ' '} {mode} {disabled}")
+
+        # create a page system
+        resume_text = ""
+        for line in resume:
+            if len(resume_text) + len(line) < 4000:
+                resume_text += line + "\n"
+
+        resume_text = "```py\n" + resume_text + "\n```"
+        codes_emb = Embed(color=0x2b2d31, description=resume_text)
+        codes_emb.set_author(name="Codes - Admin view")
+        await self.view.message.edit(embed=codes_emb, view=self.view)
+        await interaction.response.send_message("\u200b", ephemeral=True, delete_after=0.1)
+
+
+class AdminCodesToggleButton(Button):
+    def __init__(self):
+        super().__init__(
+            label="Toggle",  # get_text("view.admin.codes.toggle", ""),
+            style=ButtonStyle.grey,
+            custom_id="admin:codes:toggle"
+        )
+
+    async def callback(self, interaction: Interaction):
+        await interaction.response.send_modal(AdminCodesModal("Codes toggle", toggle_callback))
+
+
+async def toggle_callback(interaction: Interaction, codes_to_toggle):
+    codes = get_codes_data()
+    resume = []
+    for code in codes_to_toggle:
+        if code in codes:
+            status = codes[code]['disabled']
+            codes[code]['disabled'] = not status
+            resume.append(f"{'+' if status else '-'} {code}")
+        else:
+            resume.append(f"? {code}")
+
+    with open("json/codes.json", "w", encoding="utf-8") as code_file:
+        json_dump(codes, code_file, indent=2)
+    await interaction.response.send_message(f"```diff\n" + "\n".join(resume) + "\n```", ephemeral=True)
+
+
+class AdminCodesDetailButton(Button):
+    def __init__(self):
+        super().__init__(
+            label="Details",  # get_text("view.admin.codes.details", ""),
+            style=ButtonStyle.blurple,
+            custom_id="admin:codes:details"
+        )
+
+    async def callback(self, interaction: Interaction):
+        await interaction.response.send_modal(AdminCodesModal("Codes details", details_callback))
+
+
+async def details_callback(interaction: Interaction, codes_to_detail):
+    codes = get_codes_data()
+    embeds = []
+    failed = []
+
+    if len(codes_to_detail) > 10:
+        codes_to_detail = codes_to_detail[:10]
+        limit_exceeded = True
+
+    else:
+        limit_exceeded = False
+
+    for code in codes_to_detail:
+        """
+            "money": {
+                "effect": "money",
+                "extra": 50,
+                "usage_limit": [
+                  -1,
+                  "all"
+                ],
+                "used_by": {
+                  "444504367152889877": [
+                    1690635568,
+                    1690635588
+                  ]
+                },
+                "usage_count": 2,
+                "disabled": true
+            }
+        """
+        if code in codes:
+            code_data = codes[code]
+            user_count = len(code_data['used_by'])
+            emb_desc = f"**Identifier :** `{code}`\n" \
+                       f"**Effect :** `{code_data['effect']}`\n" \
+                       f"**Used :** `{code_data['usage_count']}` time{'s' if code_data['usage_count'] > 1 else ''} " \
+                       f"by `{user_count}` user{'s' if user_count > 1 else ''}\n" \
+                       f"**Usage limits :** `{code_data['usage_limit'][0]}` (`{code_data['usage_limit'][1]}`)\n" \
+                       f"**Disabled : ** `{code_data['disabled']}`"
+            emb = Embed(color=0x2b2d31, description=emb_desc).set_author(name=f"Code details - {code}")
+            embeds.append(emb)
+
+        else:
+            failed.append(code)
+
+    warning_message = ":warning: Only 10 codes can be detailed at once" if limit_exceeded else ""
+    failed_message = f"\n:warning:  The following codes couldn't be found :\n> {', '.join(failed)}" if len(failed) > 0 else ""
+    await interaction.response.send_message(warning_message + failed_message, embeds=embeds, ephemeral=True)
